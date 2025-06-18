b85602942f5d60a6851828c5b9e076e5a1dd3837
Adrien Figard <97508163+afigard@users.noreply.github.com>
2025-06-18 09:28:08 +0200

    ✨ Basic features

---
+++ lib/advisoryData.ts
@@ -0,0 +1,19 @@
+const advisoryData = [
+  {
+    country: "France",
+    alert: "Increased caution due to protests.",
+    level: "Level 2",
+  },
+  {
+    country: "Mexico",
+    alert: "High crime rate in certain regions.",
+    level: "Level 3",
+  },
+  {
+    country: "Japan",
+    alert: "Typhoon season may impact travel.",
+    level: "Level 1",
+  },
+];
+
+export default advisoryData;
